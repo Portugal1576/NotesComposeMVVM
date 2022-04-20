430b1af1d86db3f4b4bcf48ec0fa253d59d1ab52
Portugal1576 <a0975550085@gmail.com>
2022-04-20 22:01:44 +0100

    create NavController

---
+++ app/src/main/java/com/portugal1576/notescomposemvvm/navigation/NotesNavHost.kt
@@ -0,0 +1,30 @@
+package com.portugal1576.notescomposemvvm.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.portugal1576.notescomposemvvm.screens.Add
+import com.portugal1576.notescomposemvvm.screens.Main
+import com.portugal1576.notescomposemvvm.screens.Note
+import com.portugal1576.notescomposemvvm.screens.Start
+
+sealed class NavRoute(val route: String){
+    object Start: NavRoute("start_screen")
+    object Main: NavRoute("main_screen")
+    object Add: NavRoute("add_screen")
+    object Note: NavRoute("note_screen")
+}
+
+
+@Composable
+fun NotesNavHost() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = NavRoute.Start.route){
+        composable(NavRoute.Start.route){Start(navController = navController)}
+        composable(NavRoute.Main.route){Main(navController = navController)}
+        composable(NavRoute.Add.route){Add(navController = navController)}
+        composable(NavRoute.Note.route){Note(navController = navController)}
+    }
+}
---
+++ app/src/main/java/com/portugal1576/notescomposemvvm/screens/Main.kt
@@ -0,0 +1,8 @@
+package com.portugal1576.notescomposemvvm.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun Main(navController: NavHostController){
+}
